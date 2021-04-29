1455ae2910ac57d8d37154fd1828eff68696e7e0
Your Name <you@example.com>
2021-04-29 23:42:59 +0530

    play till reached 100th position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,17 +7,21 @@ public class SnakeAndLadder
         int diceroll = (int) Math.floor(((Math.random() * 10) % 6) + 1);
 	int dicecheck = (int) Math.floor(((Math.random() * 10) % 3) + 1);
 
-           if (dicecheck == 1)
-           {
-               position=position+diceroll;
-           }
-           else if (dicecheck==2)
-           {
-               position = position - diceroll;
-           }
-           else
-           {
-               position=position;
-           }
+           while (position != 100)
+        {
+            if (dicecheck == 1)
+            {
+                position=position+diceroll;
+            }
+            else if (dicecheck==2)
+            {
+                position = position - diceroll;
+            }
+            else
+            {
+                position=position;
+            }
 
+	}
+    }
 }
