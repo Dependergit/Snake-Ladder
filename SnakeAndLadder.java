8ab219bb8ed08101cf6ca373d742f3d516ff02f8
Your Name <you@example.com>
2021-04-29 23:54:34 +0530

    refactor or edit file with some line of code inside while loop

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,11 +4,12 @@ public class SnakeAndLadder
     public static void main(String[] args)
     {
         int position = 0;
-        int diceroll = (int) Math.floor(((Math.random() * 10) % 6) + 1);
-	int dicecheck = (int) Math.floor(((Math.random() * 10) % 3) + 1);
 
            while (position != 100)
         {
+	    int diceroll = (int) Math.floor(((Math.random() * 10) % 6) + 1);
+            int dicecheck = (int) Math.floor(((Math.random() * 10) % 3) + 1);
+
             if (dicecheck == 1)
            {
                if (position+diceroll<=100)
